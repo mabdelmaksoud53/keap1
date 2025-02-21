16e7eafc20b77cefea567d960f37deb3b22297a3
Mostafa Sayed <167385230+mabdelmaksoud53@users.noreply.github.com>
2025-02-21 22:50:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ import pickle
 
 # Molecular descriptor calculator
 def desc_calc():
-    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
-    os.remove('molecule.smi')
+    
+    if error:
+        st.error("Error running PaDEL-Descriptor. Ensure Java is installed and paths are correct.")
+    
+    if os.path.exists("molecule.smi"):
+        os.remove("molecule.smi")
 
 # File download function
 def filedownload(df):
@@ -22,40 +27,59 @@ def filedownload(df):
 
 # Model building function
 def build_model(input_data):
-    load_model = pickle.load(open('keap1_model.pkl', 'rb'))
-    prediction = load_model.predict(input_data)
-    st.header('**Prediction output**')
-    prediction_output = pd.Series(prediction, name='pIC50')
-    molecule_name = pd.Series(load_data[1], name='molecule_name')
-    df = pd.concat([molecule_name, prediction_output], axis=1)
-    st.write(df)
-    st.markdown(filedownload(df), unsafe_allow_html=True)
+    try:
+        with open('keap1_model.pkl', 'rb') as file:
+            load_model = pickle.load(file)
+        
+        prediction = load_model.predict(input_data)
+        st.header('**Prediction output**')
+        
+        prediction_output = pd.Series(prediction, name='pIC50')
+        molecule_name = pd.Series(load_data.iloc[:, 0], name='molecule_name')
+        df = pd.concat([molecule_name, prediction_output], axis=1)
+        
+        st.write(df)
+        st.markdown(filedownload(df), unsafe_allow_html=True)
+    except Exception as e:
+        st.error(f"Error in model prediction: {e}")
 
 # Streamlit UI
 st.markdown("# Bioactivity Prediction App (Keap-1 Inhibitor)")
 st.sidebar.header('1. Upload your CSV data')
+
 uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['txt'])
 
 if st.sidebar.button('Predict'):
-    load_data = pd.read_table(uploaded_file, sep=' ', header=None)
-    load_data.to_csv('molecule.smi', sep='\t', header=False, index=False)
-    st.header('**Original input data**')
-    st.write(load_data)
+    if uploaded_file is not None:
+        load_data = pd.read_table(uploaded_file, sep=' ', header=None)
+        load_data.to_csv('molecule.smi', sep='\t', header=False, index=False)
+        
+        st.header('**Original input data**')
+        st.write(load_data)
 
-    with st.spinner("Calculating descriptors..."):
-        desc_calc()
+        with st.spinner("Calculating descriptors..."):
+            desc_calc()
 
-    st.header('**Calculated molecular descriptors**')
-    desc = pd.read_csv('descriptors_output.csv')
-    st.write(desc)
-    st.write(desc.shape)
+        if os.path.exists('descriptors_output.csv'):
+            st.header('**Calculated molecular descriptors**')
+            desc = pd.read_csv('descriptors_output.csv')
+            st.write(desc)
+            st.write(desc.shape)
 
-    st.header('**Subset of descriptors from previously built models**')
-    Xlist = list(pd.read_csv('descriptor_list.csv').columns)
-    desc_subset = desc[Xlist]
-    st.write(desc_subset)
-    st.write(desc_subset.shape)
-
-    build_model(desc_subset)
+            st.header('**Subset of descriptors from previously built models**')
+            
+            if os.path.exists('descriptor_list.csv'):
+                Xlist = list(pd.read_csv('descriptor_list.csv').columns)
+                desc_subset = desc[Xlist]
+                st.write(desc_subset)
+                st.write(desc_subset.shape)
+                
+                build_model(desc_subset)
+            else:
+                st.error("Missing 'descriptor_list.csv'. Ensure it is in the correct directory.")
+        else:
+            st.error("PaDEL-Descriptor failed. Check if Java is installed.")
+    else:
+        st.error("Please upload a valid text file.")
 else:
     st.info('Upload input data in the sidebar to start!')
