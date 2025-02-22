1effd9767a48983db473c6adfda6fe558159768a
Mostafa Sayed <167385230+mabdelmaksoud53@users.noreply.github.com>
2025-02-22 18:09:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,119 +1,86 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 import subprocess
 import os
-import joblib
-from rdkit import Chem
-from rdkit.Chem import Descriptors
-from rdkit.ML.Descriptors import MoleculeDescriptors
-from rdkit.Chem import Draw
-from io import BytesIO
 import base64
+import pickle
 
-st.set_page_config(
-    page_title="Virtual Screening")
-
-
-st.header("Virtual Screening")
-st.warning("""
-[Click here to see example for input format](https://drive.google.com/file/d/1Qa-0JKVa9ccuSMd4Km6Z5nP-RLeT1Gp0/view?usp=sharing)
-""")
-
-def PUbchemfp_desc_calc():
+# Molecular descriptor calculator
+def desc_calc():
     # Performs the descriptor calculation
-    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -detectaromaticity -fingerprints  -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     os.remove('molecule.smi')
-    
-def descriptors(smiles):
-    mols = [Chem.MolFromSmiles(s) for s in smiles] 
-    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in Descriptors._descList])
-    desc_names = calc.GetDescriptorNames()
-    
-    Mol_descriptors =[]
-    for mol in mols:
 
-        descriptors = calc.CalcDescriptors(mol)
-        Mol_descriptors.append(descriptors)
-    return Mol_descriptors,desc_names 
+# File download
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    return href
 
-def smiles_to_image(smiles, mol_size=(200, 200)):
-    mol = Chem.MolFromSmiles(smiles)
-    if mol:
-        img = Draw.MolToImage(mol, size=mol_size)
-        buffer = BytesIO()
-        img.save(buffer, format="PNG")
-        buffer.seek(0)
-        img_str = base64.b64encode(buffer.read()).decode()
-        return f'<img src="data:image/png;base64,{img_str}" alt="Molecule">'
-    else:
-        return "Invalid SMILES"	
-    
-
-# Model
-def the_model(input_data):
-    load_model = joblib.load('keap1_model.pkl')
-    # Make prediction
+# Model building
+def build_model(input_data):
+    # Reads in saved regression model
+    load_model = pickle.load(open('keap1_model.pkl', 'rb'))
+    # Apply model to make predictions
     prediction = load_model.predict(input_data)
-    prediction_probability=load_model.predict_proba(input_data)
-    
-    x=pd.DataFrame(prediction_probability,columns=["Pi","Pa"])
-    st.header('Prediction Result')
-    
-    prediction_output = pd.Series(prediction, name='Result')
-
-    #proba_output=pd.Series(prediction_probability,name="prediction_proba")
-    
-    molecule_name = pd.Series(reading_data["Molecule Name"], name='Molecule Name')
-    
-    Result= pd.concat([molecule_name, x, prediction_output], axis=1)
-    
-    result = []
-    for x in Result["Result"]:
-        if x==1:
-            result.append("Active")
-        if x==0:
-            result.append("Inactive")
-    Result["Result"]=result
-    st.write(Result)
-    prediction_csv = Result.to_csv(index=False,sep=",")
-    st.download_button(label="Download prediction results",data=prediction_csv,file_name="vs_results.csv")
-
-
-
-uplouded_file=st.file_uploader("Please upload your input file", type=['txt'])
-
-
-if st.button('Predict'):
-    reading_data = pd.read_table(uplouded_file, sep=' ', names=["Smiles","Molecule Name"])
-    reading_data.to_csv('molecule.smi', sep = '\t', index = False, header=None)
-    reading_data['Structure'] = reading_data['Smiles'].apply(smiles_to_image)
-    st.subheader('Input data')
-    st.markdown(reading_data.to_html(escape=False, index=False), unsafe_allow_html=True)
-   ## st.write(reading_data)
-
-
-
-    PUbchemfp_desc_calc()
-
- 
-    st.subheader('Generated PubChem_Fingerprints')
-    pubfp_calc = pd.read_csv("descriptors_output.csv")
-    pubfp_calc.drop('Name', axis=1, inplace=True)
-    st.write(pubfp_calc)
-    st.write(pubfp_calc.shape)
-   
-    first_column = reading_data.iloc[:, 0] 
-    Mol_descriptors,desc_names =descriptors(first_column)
-    df_with_200_descriptors = pd.DataFrame(Mol_descriptors,columns=desc_names)
-    df=df_with_200_descriptors[["MolWt","MolLogP","NumHAcceptors","NumHDonors"]]
-    st.subheader("Lipinski Rule of 5 Descriptors")
+    st.header('**Prediction output**')
+    prediction_output = pd.Series(prediction, name='pIC50')
+    molecule_name = pd.Series(load_data[1], name='molecule_name')
+    df = pd.concat([molecule_name, prediction_output], axis=1)
     st.write(df)
-    
-    
-    the_model(pubfp_calc)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
+
+# Logo image
+# image = Image.open('logo.png')
+
+# st.image(image, use_column_width=True)
+
+# Page title
+st.markdown("""
+# Bioactivity Prediction App (keap-1)
+
+This app allows you to predict the bioactivity towards inhibting the `coronavirus` enzyme.
+**Credits**
+- App built in `Python` + `Streamlit` by [Mostafa Sayed](https://github.com/mabdelmaksoud53)
+- Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
+---
+""")
+
+# Sidebar
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['txt'])
+    st.sidebar.markdown("""
+[Example input file](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)
+""")
+
+if st.sidebar.button('Predict'):
+    load_data = pd.read_table(uploaded_file, sep=' ', header=None)
+    load_data.to_csv('molecule.smi', sep = '\t', header = False, index = False)
+
+    st.header('**Original input data**')
+    st.write(load_data)
+
+    with st.spinner("Calculating descriptors..."):
+        desc_calc()
+
+    # Read in calculated descriptors and display the dataframe
+    st.header('**Calculated molecular descriptors**')
+    desc = pd.read_csv('descriptors_output.csv')
+    st.write(desc)
+    st.write(desc.shape)
+
+    # Read descriptor list used in previously built model
+    st.header('**Subset of descriptors from previously built models**')
+    Xlist = list(pd.read_csv('descriptor_list.csv').columns)
+    desc_subset = desc[Xlist]
+    st.write(desc_subset)
+    st.write(desc_subset.shape)
+
+    # Apply trained model to make prediction on query compounds
+    build_model(desc_subset)
 else:
-    st.warning('Limit 250 compounds per file')
-    
-    
+    st.info('Upload input data in the sidebar to start!')
